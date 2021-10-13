7db12967a70110054330f8218406a2d2813ca833
Álvaro <correo@laultimapregunta.com>
2021-10-13 10:37:10 +0200

    Instanciar columnas iniciales
    He creado un método que crea columnas entre nuestra posición y la del instanciador. Lo hace de forma automática, calculando la distancia y el nº de columnas a crear en base a la separación entre ellas

--- PR_ZAXXON_HolgueraAlvaro/Assets/Scripts/Juego/IntanciadorObst.cs
+++ PR_ZAXXON_HolgueraAlvaro/Assets/Scripts/Juego/IntanciadorObst.cs
@@ -19,7 +19,10 @@ public class IntanciadorObst : MonoBehaviour
     int level;
     //el intervalo para la corrutina, dependerá de la velocidad
     float intervalo;
+    //Variable con la distancia entre obstáculos
     [SerializeField] float distanciaEntreObstaculos;
+    //Variable para las columnas iniciales
+    [SerializeField] float distPrimerObst = 60f; //Lo declaro aquí para poder cambiarlo en Unity
 
     //Para calcular el intervalo, necesito saber la velocidad
     float speed;
@@ -34,7 +37,10 @@ public class IntanciadorObst : MonoBehaviour
 
         distanciaEntreObstaculos = 30f;
         intervalo = distanciaEntreObstaculos / initGameScript.spaceshipSpeed;
-        print(intervalo);
+
+        //Llamo al método que crea columnas iniciales
+        CrearColumnasIniciales();
+
         //Inicio la Corrutina que instancia los prefabs
         //IMPORTANTE: antes de iniciarla, el intervalo tiene que ser un número válido
         StartCoroutine("CrearObstaculos");
@@ -91,12 +97,36 @@ public class IntanciadorObst : MonoBehaviour
 
             //Antes de instanciar calculo el intervalo en basde a la velocidad
             intervalo =  distanciaEntreObstaculos / initGameScript.spaceshipSpeed;
-            print(intervalo);
+            //print(intervalo);
 
             //Instancio el prefab aleatorio en la posición calculada
             Instantiate(arrayObst[randomNum], instPos, Quaternion.identity);
 
             yield return new WaitForSeconds(intervalo);
+
+        }
+    }
+
+    //Este método lo he creado para crear unas columnas iniciales
+    void CrearColumnasIniciales()
+    {
+
+        //El nº de obstáculos es la resta de dónde estoy menos el primero, partido por la distancia entre obstáculos
+        float numColumnasIniciales = (transform.position.z - distPrimerObst) / distanciaEntreObstaculos;
+        //Por si acaso sale un nº con decimales lo redondeo con Mathf.Round
+        numColumnasIniciales = Mathf.Round(numColumnasIniciales); //Redondeo el número
+        //print("Nº de columnas iniciales: " + numColumnasIniciales);
+        
+        //Creo un bucle pero en vez de sumar 1 lo hago para que me de la posición en Z de cada obstáculo
+        //la variable n vale al principio la distancia del primer obstáculo
+        //Mientras n no llegue a dónde estoy en Z, seguirá sumando la distancia entre obstáculos
+        //De esta forma n es igual a la posición en Z donde tengo que instanciar
+        for (float n = distPrimerObst; n < transform.position.z; n += distanciaEntreObstaculos)
+        {
+            //Creo la posición de instanciación, con los valores aleatorios y n en la posición en Z
+            Vector3 initColPos = new Vector3(Random.Range(-10f, 10f), 0f, n);
+            //Creo el obstáculo (en mi caso, al ser un array, creo el primero del array
+            Instantiate(arrayObst[0], initColPos, Quaternion.identity);
 
         }
     }
